f771eb01d8065e7788654331aa07bc52f39dee29
maia1dev <gabrielmaiaa22@gmail.com>
2022-11-07 23:12:01 -0300

    Joga da imitação.

---
+++ JS/Projeto01/Functions/functions.js
@@ -0,0 +1,12 @@
+// Criar um aplicativo de frases motivacionais
+
+
+
+function createPhrases() {
+  console.log("A persistência é o caminho do êxito.")
+  console.log("As pessoas costumam dizer que a motivação não dura sempre. Bem, nem o efeito do banho, por isso recomenda-se diariamente.")
+  console.log("Motivação é a arte de fazer as pessoas fazerem o que você quer que elas façam porque elas o querem fazer.")
+}
+
+createPhrases()
+createPhrases()
