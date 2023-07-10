ad9a94aedf2985aaf43d91e7a98bfa94b53ea2a2
GMaiaa <gabrielmaiaa22@gmail.com>
2023-07-10 20:46:35 -0300

    Finishes RocketNotes Frontend

--- RocketNotes/src/pages/SignUp/index.jsx
+++ RocketNotes/src/pages/SignUp/index.jsx
@@ -1,33 +1,36 @@
 import { Container, Form, Background } from "./styles";
-import { Link } from "react-router-dom";
-import { FiMail, FiLock } from "react-icons/fi";
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
 import { Button } from "../../components/Button"
+import { Link } from "react-router-dom";
  
 import { Input } from "../../components/Input";
 
 
-export function SignIn() {
+export function SignUp() {
   return (
     <Container>
+      <Background/>
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicaçãp para salvar e gerenciar seus links úteis</p>
 
-        <h2>Faça seu login</h2>
+        <h2>Crie sua conta</h2>
+
+        <Input placeholder="Nome" type="text" icon={FiUser} />
 
         <Input placeholder="E-mail" type="text" icon={FiMail} />
 
         <Input placeholder="Senha" type="password" icon={FiLock} />
 
-        <Button title="Entrar"/>
+        <Button title="Cadastrar"/>
 
-        <Link to="/register">
-            Criar conta
+        <Link to="/">
+            Voltar para o login
         </Link>
            
         
       </Form>
-      <Background/>
+      
     </Container>
   );
 }
