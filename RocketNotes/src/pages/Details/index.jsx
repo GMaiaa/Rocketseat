9f3a81d0f8a305041d66911516b9487732cb641e
GMaiaa <gabrielmaiaa22@gmail.com>
2023-07-10 12:27:53 -0300

    Starts RocketNotes FrontEnd

---
+++ RocketNotes/src/pages/Details/index.jsx
@@ -0,0 +1,48 @@
+import { Container, Links, Content } from "./styles";
+
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
+
+export function Details() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1> Introdução ao React </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+            reiciendis tempora enim nulla est deserunt aperiam dolores,
+            distinctio maxime sint officiis alias qui tempore vitae amet
+            sapiente ullam possimus accusamus.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
+    </Container>
+  );
+}
